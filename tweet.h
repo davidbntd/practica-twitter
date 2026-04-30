8f589ccdfa24264fb7a12cf4886a741f450b4699
davidbntd <d.benitez.2025@alumnos.urjc.es>
2026-04-30 12:26:52 +0200

    Create tweet.h

---
+++ tweet.h
@@ -0,0 +1,11 @@
+//
+// Created by d.benitez.2025 on 24/04/2026.
+//
+
+#ifndef TWEET_H
+#define TWEET_H
+
+typedef char ttweet [140];
+
+void asignartweet(ttweet* destino, ttweet origen);
+void mostrartweet(ttweet tweet);
