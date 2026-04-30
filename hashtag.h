8f3f3fc1fbafe9b9716ea22ed23ec38917521532
davidbntd <d.benitez.2025@alumnos.urjc.es>
2026-04-30 12:26:09 +0200

    Create hashtag.h

---
+++ hashtag.h
@@ -0,0 +1,17 @@
+//
+// Created by d.benitez.2025 on 24/04/2026.
+//
+
+#ifndef HASHTAG_H
+#define HASHTAG_H
+
+typedef char thastagh[100];
+
+int comparar(thastagh a,thastagh b);
+void mostrarhasstag(thastagh a);
+int eshashtagMayor(thastagh a, thastagh b);
+
+
+
+#endif HASHTAG_H
+#endif HASHTAG_H
